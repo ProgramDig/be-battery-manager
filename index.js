9a05a3c0d0af75e094a4e15821969f26cbd0001e
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2023-06-09 11:15:55 +0300

    (feat) - add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const {connect, set} = require('mongoose')
+const cors = require("cors")
 
 const PORT = process.env.PORT || 5000
 const app = express({extended: true})
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth.router'))
